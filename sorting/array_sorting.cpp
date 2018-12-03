7e845f182f4d14dba96865ea7ffd626b8df712bb
adityapande-1995 <aditya0509951@gmail.com>
2018-12-03 18:10:12 +0530

    Started sorting algos 2

--- sorting/array_sorting.cpp
+++ sorting/array_sorting.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-void BubbleSort(std::vector<int> a){
+std::vector<int> BubbleSort(std::vector<int> a){
     int n = a.size();
     for (int j = 1; j < n; j++){
         for (int i = 0; i < (n-j); i++){
@@ -13,18 +13,45 @@ void BubbleSort(std::vector<int> a){
             }
         }
     }
+
+    return a;
+}
+
+std::vector<int> SelectionSort(std::vector<int> a){
+    int n = a.size();
+    int smallest_no, smallest_index;
+    for (int i=0; i<(n-1); i++){
+        smallest_index = i;
+        smallest_no = a[smallest_index];
+        // Find smallest number
+        for (int j = (i+1); j<n; j++){
+            if(smallest_no > a[j]){
+                smallest_no = a[j];
+                smallest_index = j;
+            }
+        }
+
+        // Swap smallest no and i
+        if (smallest_index != i){
+            int temp = a[smallest_index];
+            a[smallest_index] = a[i];
+            a[i] = temp;
+        }
+    }
+
+    return a;
 }
 
 int main(){
     std::vector<int> a = {100,99,98,97,96,10,31,5}; 
 
-    //Bubble sort
-    BubbleSort(a);
+    // auto b = BubbleSort(a);
+    auto b = SelectionSort(a);
 
 
     // Print array
-    for (int i =0; i < a.size(); i++){
-        std::cout << a[i] << std::endl;
+    for (int i =0; i < b.size(); i++){
+        std::cout << b[i] << std::endl;
     }
     
 
